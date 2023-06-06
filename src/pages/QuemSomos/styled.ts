dbafcb3eb5cb59575d3710a4f449c3ffe2a34adb
Phelipe Lani <p.lani@live.com>
2023-06-06 13:14:35 -0300

    fix:page-Quem-somos

--- src/pages/QuemSomos/styled.ts
+++ src/pages/QuemSomos/styled.ts
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import background from "../../assets/sindico-profissional_BACK_gROUNND.jpg";
 
 export const ContainerMain = styled.div`
-  background: linear-gradient(
-      rgba(0, 0, 0, 0.5),  /* Defina o valor de opacidade desejado aqui */
-      rgba(0, 0, 0, 0.5)
-    ),
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url(${background}) center/cover no-repeat;
+  width: 100vw;
+  height: 80vh;
+  object-fit:cover;
 `;
+
